78bf7d0cec680d7415725ba8a79c9fc106caa062
hexenq <hexenq@gmail.com>
2018-03-26 18:13:14 +0800

    20180326

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 import kuromoji from "kuromoji";
 
-function Analyzer({ dicPath } = {}) {
-    this._analyzer = null;
+class Analyzer {
+    constructor({ dicPath } = {}) {
+        this._analyzer = null;
 
-    if (!dicPath) {
-        this._dicPath = require.resolve('kuromoji').replace(/src(?!.*src).*/, 'dict/');
-    } else {
-        this._dicPath = dicPath;
+        if (!dicPath) {
+            this._dicPath = require.resolve('kuromoji').replace(/src(?!.*src).*/, 'dict/');
+        } else {
+            this._dicPath = dicPath;
+        }
+    }
+
+    init(callback = () => {}) {
+        let self = this;
+        if (this._analyzer == null) {
+            kuromoji.builder({ dicPath: this._dicPath }).build(function (err, newAnalyzer) {
+                if (err)
+                    return callback(err);
+
+                self._analyzer = newAnalyzer;
+                callback();
+            });
+        } else {
+            callback(new Error("This analyzer has already been initialized."));
+        }
+    }
+
+    parse(str = "", callback = () => {}) {
+        if (str.trim() == "") return callback(null, str);
+        callback(null, this._analyzer.tokenize(str));
     }
 }
-
-Analyzer.prototype.init = function (callback) {
-    let self = this;
-    if (this._analyzer == null) {
-        kuromoji.builder({ dicPath: this._dicPath }).build(function (err, newAnalyzer) {
-            if (err)
-                return callback(err);
-
-            self._analyzer = newAnalyzer;
-            callback();
-        });
-    } else {
-        callback(new Error("This analyzer has already been initialized."));
-    }
-};
-
-Analyzer.prototype.parse = function (str, callback) {
-    if (str.trim() == "") return callback(null, str);
-    callback(null, this._analyzer.tokenize(str));
-};
 
 export default Analyzer;
